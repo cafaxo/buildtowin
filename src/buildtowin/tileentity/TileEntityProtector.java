ceb9599c670ae8764a42f6fea8da056d18e9627e
cafaxo <cafaxo@gmail.com>
2013-04-22 15:49:03 +0200

    removed dead code

--- src/buildtowin/tileentity/TileEntityProtector.java
+++ src/buildtowin/tileentity/TileEntityProtector.java
@@ -8,8 +8,6 @@ import net.minecraft.nbt.NBTTagCompound;
 
 public class TileEntityProtector extends TileEntitySynchronized {
     
-    private TileEntityTeamHub teamHub;
-    
     private int radius;
     
     public TileEntityProtector() {
