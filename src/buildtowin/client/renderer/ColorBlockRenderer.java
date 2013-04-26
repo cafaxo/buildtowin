8135a6187a5e9cfd1a4c54286286d8d2a3dc4b57
cafaxo <cafaxo@gmail.com>
2013-04-26 17:34:29 +0200

    fixed inventory rendering of colored blocks

--- src/buildtowin/client/renderer/ColorBlockRenderer.java
+++ src/buildtowin/client/renderer/ColorBlockRenderer.java
@@ -3,7 +3,11 @@ package buildtowin.client.renderer;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.world.IBlockAccess;
+
+import org.lwjgl.opengl.GL11;
+
 import buildtowin.BuildToWin;
 import buildtowin.util.Color;
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
@@ -14,7 +18,46 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class ColorBlockRenderer implements ISimpleBlockRenderingHandler {
     
     @Override
-    public void renderInventoryBlock(Block block, int metadata, int modelID, RenderBlocks renderer) {
+    public void renderInventoryBlock(Block par1Block, int metadata, int modelID, RenderBlocks renderer) {
+        par1Block.setBlockBoundsForItemRender();
+        renderer.setRenderBoundsFromBlock(par1Block);
+        
+        GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
+        GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
+        
+        Tessellator tessellator = Tessellator.instance;
+        
+        tessellator.startDrawingQuads();
+        tessellator.setNormal(0.0F, -1.0F, 0.0F);
+        renderer.renderBottomFace(par1Block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(par1Block, 0, metadata));
+        tessellator.draw();
+        
+        tessellator.startDrawingQuads();
+        tessellator.setNormal(0.0F, 1.0F, 0.0F);
+        renderer.renderTopFace(par1Block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(par1Block, 1, metadata));
+        tessellator.draw();
+        
+        tessellator.startDrawingQuads();
+        tessellator.setNormal(0.0F, 0.0F, -1.0F);
+        renderer.renderEastFace(par1Block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(par1Block, 2, metadata));
+        tessellator.draw();
+        
+        tessellator.startDrawingQuads();
+        tessellator.setNormal(0.0F, 0.0F, 1.0F);
+        renderer.renderWestFace(par1Block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(par1Block, 3, metadata));
+        tessellator.draw();
+        
+        tessellator.startDrawingQuads();
+        tessellator.setNormal(-1.0F, 0.0F, 0.0F);
+        renderer.renderNorthFace(par1Block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(par1Block, 4, metadata));
+        tessellator.draw();
+        
+        tessellator.startDrawingQuads();
+        tessellator.setNormal(1.0F, 0.0F, 0.0F);
+        renderer.renderSouthFace(par1Block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(par1Block, 5, metadata));
+        tessellator.draw();
+        
+        GL11.glTranslatef(0.5F, 0.5F, 0.5F);
     }
     
     @Override
@@ -45,7 +88,7 @@ public class ColorBlockRenderer implements ISimpleBlockRenderingHandler {
     
     @Override
     public boolean shouldRender3DInInventory() {
-        return false;
+        return true;
     }
     
     @Override
