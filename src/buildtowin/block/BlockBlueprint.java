15040b6636f9d69905fbc5c4cc4b0ad82e37e418
cafaxo <cafaxo@gmail.com>
2013-04-27 12:32:39 +0200

    fixed detection of correct blueprint replacement

--- src/buildtowin/block/BlockBlueprint.java
+++ src/buildtowin/block/BlockBlueprint.java
@@ -51,7 +51,8 @@ public class BlockBlueprint extends BlockContainer {
         }
         
         if (entityPlayer.inventory.getCurrentItem() != null) {
-            if (entityPlayer.inventory.getCurrentItem().itemID == Block.blocksList[blockData.savedId].idDropped(0, new Random(), 0)) {
+            if (entityPlayer.inventory.getCurrentItem().itemID == blockData.savedId
+                    || entityPlayer.inventory.getCurrentItem().itemID == Block.blocksList[blockData.savedId].idDropped(0, new Random(), 0)) {
                 if (blockData.metadata == 0) {
                     entityPlayer.worldObj.spawnEntityInWorld(new EntityXPOrb(entityPlayer.worldObj, x, y, z, 10));
                     blockData.metadata = 1;
